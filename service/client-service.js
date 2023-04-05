ec88d30ea3d6b3dc504a7e2096485b6773913693
Gabriel Martinez <martinegabriel47@gmail.com>
2023-04-05 12:16:41 -0500

    Update client-service.js

--- service/client-service.js
+++ service/client-service.js
@@ -1,3 +1,34 @@
+const crearNuevaLinea = (nombre, email) => {
+    const linea = document.createElement("tr");
+    const contenido = `
+            <td class="td" data-td>${nombre}</td>
+            <td>${email}</td>
+            <td>
+              <ul class="table__button-control">
+                <li>
+                  <a
+                    href="../screens/editar_cliente.html"
+                    class="simple-button simple-button--edit"
+                    >Editar</a
+                  >
+                </li>
+                <li>
+                  <button
+                    class="simple-button simple-button--delete"
+                    type="button"
+                  >
+                    Eliminar
+                  </button>
+                </li>
+              </ul>
+            </td>
+          `
+          linea.innerHTML = contenido;
+          return linea;
+};
+
+const table = document.querySelector("[data-table]");
+
 const http = new XMLHttpRequest();
 
 //Abrir HTTP  puede recibir (metodo, url)
@@ -7,9 +38,20 @@ Read = GET
 Update = PUT/PATCH
 Delete = DELETE
 */
-http.open("GET", "http://localhost:3000/perfil");
+http.open("GET", "http://localhost:3000/perfil")
 http.send();
+
 http.onload = () => {
-    const data = http.response;
+    const data = JSON.parse(http.response);
     console.log(data);
+    data.forEach((perfil) => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        table.appendChild(nuevaLinea);
+    });
+    const http2 = new XMLHttpRequest();
+    http2.open("GET", "http://localhost:3000/perfil/hoy")
+    http2.send();
+    http2.onload = () => {
+        const data2 = JSON.parse(http2.response);
+    }
 }
